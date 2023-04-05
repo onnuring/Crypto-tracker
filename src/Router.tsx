fdd6cf8105c3090f11fe741aea8cd62162f51f63
onnuri <mathilda0922@gmail.com>
2023-04-05 16:19:45 +0900

    error fix

--- src/Router.tsx
+++ src/Router.tsx
@@ -43,7 +43,6 @@ const router = createBrowserRouter([
           {
             path: "price",
             element: <Price />,
-            errorElement: <CoinError />,
           },
         ],
       },
